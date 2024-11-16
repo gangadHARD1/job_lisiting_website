38dd1657c2c603618f6ccba299d621cc26802406
champion <beraanunay@gmail.com>
2024-11-16 15:09:40 +0530

    added functionality to edit and remove jobs, created the profuction build

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,53 @@ import {Route,createBrowserRouter,createRoutesFromElements,RouterProvider
 import JobsPage from './pages/JobsPage';
 import ErrorPage from './pages/ErrorPage';
 import Job_expanded, {jobLoader} from './pages/job_expanded';
- 
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 {/* index element means its goonna be the home page so nop need ot specify path*/}
 
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element= {<MainLayout/>}>
-    <Route index element={<HomePage/>} />
-    <Route path='/jobs' element={<JobsPage/>}/>
-    <Route path='/jobs/:id' element={<Job_expanded/>} loader={jobLoader}/>   {/* the colon after jobs/ signifies this is dynamic*/}
-    <Route path='*' element={<ErrorPage/>}/>
-    </Route>
-  )
-);
 
 const App = () => {
+  const addJob =async(newJob)=>{
+
+    const res=await fetch ('/api/jobs',{method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify(newJob)
+    })
+    return 
+  }
+
+  //for deleting job
+  const delJob= async (id)=> {
+    const res=await fetch(`/api/jobs/${id}`,{
+      method:'DELETE'
+    })
+
+  }
+  //updatejob
+  const editJob=async(job)=>{
+    const res=await fetch (`/api/jobs/${job.id}`,{
+      method:"PUT",
+      headers:{'Content-Type':'application/json',
+      },
+      body:JSON.stringify(job)})
+      return 
+    }
+       
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element= {<MainLayout/>}>
+      <Route index element={<HomePage/>} />
+      <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='/jobs/:id' element={<Job_expanded deleteJob={delJob}/>} loader={jobLoader}/>   {/* the colon after jobs/ signifies this is dynamic ALSO LOADER IS NOT A PROP!!*/}
+      <Route path='*' element={<ErrorPage/>}/>
+      <Route path='/edit-job/:id' element={<EditJobPage editJob={editJob}/>} loader={jobLoader}/>
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>}/>
+      </Route>
+    )
+  );
+
   return (
    <RouterProvider router= {router}/>
   );
